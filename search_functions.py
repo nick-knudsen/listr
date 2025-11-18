cdb303897672a98a6a1cbbeea1d171d93ac7be5a
Nick Knudsen <nick.knudsen14@gmail.com>
2025-11-18 17:15:28 -0500

    setting up duckdb

--- search_functions.py
+++ search_functions.py
@@ -8,3 +8,10 @@ search_results = unique_complete_hotspot_sightings_df[
 
 
 print(search_results)
+
+
+yellowthroat_df = sightings_df[sightings_df['COMMON NAME'] == 'Common Yellowthroat']
+yellowthroat_df['DAY OF YEAR'] = yellowthroat_df['OBSERVATION DATE'].dt.dayofyear
+late_yellowthroat_df = yellowthroat_df[yellowthroat_df['DAY OF YEAR'] >= pd.to_datetime('1970-11-01').day_of_year ].sort_values('DAY OF YEAR', ascending=False)
+late_yellowthroat_df = pd.DataFrame(late_yellowthroat_df.compute().reset_index(drop=True))
+print(late_yellowthroat_df)
